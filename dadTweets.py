49cc164946d972f8402b505c810d7005ae336c91
Kimberly A. Brink <kabrink8@gmail.com>
2016-05-18 17:25:24 -0400

    Update dadTweets.py

--- dadTweets.py
+++ dadTweets.py
@@ -1,87 +1,63 @@
-import twitter
-import oauthDance
-import json
 import csv
-from time import sleep
+import json
+import oauthDance
 import re
 import sys
+import twitter
 from alchemyapi import AlchemyAPI
+from time import sleep
 
-
+# Initialize Alchemy and Twitter communications
 alchemy = AlchemyAPI()
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
 t = oauthDance.login()
-
 count = 0
 
+#empty list for dads
 dads = []
-
-#print 'Reading dads.txt...'
 reader = csv.reader(open('dads-update.txt', 'rU'), dialect=csv.excel_tab)
 for row in reader:
+    #skip header row
     if count == 0:
         count = 1
         continue
+
     dad = {}
     dad['screen_name'] = row[0]
     dad['desc'] = row[1]
     dads.append(dad)
 
-
+# For every dad, grab 10 latest tweets
 tweets = []
-#print 'Grabbing tweets...'
 for item in range(0, len(dads)):
     try:
-        tweet = t.statuses.user_timeline(count=10, screen_name=dads[item]['screen_name'])
-        tweets.append(tweet)
+        tweetResponse = t.statuses.user_timeline(count=10, screen_name=dads[item]['screen_name'])
+        tweets.append(tweetResponse)
     except:
         pass
 
-#print tweets
-print len(tweets)
 tweetText = []
 
+# Turn data into a list of the tweet text
 for tweetResponse in tweets:
     for tweet in tweetResponse:
         tweetText.append(tweet['text'])
 
-#==============================================================================
-# fout = open('tweets2.txt', 'wb')
-# writer = csv.writer(fout, delimiter='\t')
-# for tweetResponse in tweets:
-#     for tweet in tweetResponse:
-#          writer.writerow(tweet['text'].encode('ascii', 'ignore'))
-# fout.close()
-#==============================================================================
-
-
-#tweets = []
-# with open('tweetChunks.txt', 'rb') as csvfile:
-#     reader = csv.reader(csvfile, delimiter='\t')
-#     for row in reader:
-#         if count == 0:
-#             count = 1
-#             continue
-#         tweet = row[0]
-#         tweets.append(tweet)
-
-#print tweets
-
+# Split list of tweet text into tweet chunks and do analysis
 tweetChunks = []
-
-print 'Combining tweets..'
 allText = ''
+# analyze each tweetChunk
 for i in range(0,len(tweetText)/10):
     text = ''
     for j in range(0,10):
         currentText = tweetText[(10*i)+j]
+        # Use regular expressions to remove 'RT' and urls
         noRtText = re.sub(r'\bRT\b'," ", currentText)
         noUrlText = re.sub(r"http\S+","", noRtText)
         text = text + noUrlText + ' '
         allText = allText + noUrlText + ' '
+    # Call AlchemyAPI to get keywords and sentiment from current tweetChunk
     keywords = alchemy.keywords('text', text)
     sentiment = alchemy.sentiment('text',text)
     tweet = {}
@@ -92,26 +68,21 @@ for i in range(0,len(tweetText)/10):
         tweet['keywords'] = tweet['keywords'] + word['text'] + ' ' + word['relevance'] + ' '
     tweetChunks.append(tweet)
 
+# Analyse all text together
 overallResult = {}
 overallResult['chunk'] = allText
 keywords = alchemy.keywords('text', allText)
 overallResult['keywords'] = ''
+# Call AlchemyAPI to get keywords and sentiment for all text together
 sentimentType = alchemy.sentiment('text',allText)['docSentiment']['type']
 sentimentScore = alchemy.sentiment('text',allText)['docSentiment']['score']
-overallResult['sentiment'] = sentimentType + ' ' + sentimentScore + ' ' 
+overallResult['sentiment'] = sentimentType + ' ' + sentimentScore + ' '
 for word in keywords['keywords']:
     overallResult['keywords'] = overallResult['keywords'] + word['text'] + ' ' + word['relevance'] + ' '
 
-
-print '\n\n\n'
-print tweetChunks[0]['chunk']
-print '\n'
-print tweetChunks[0]['keywords']
-print '\n\n\n'
-
+# Write processed chunks to 'tweetChunks2.txt'
 fout = open('tweetChunks2.txt', 'wb')
 writer = csv.writer(fout, delimiter='\t')
-writer.writerow(['chunk', 'keyword'])
 for item in range(0, len(tweetChunks)):
 	writer.writerow([tweetChunks[item]['chunk'], tweetChunks[item]['keywords'], tweetChunks[item]['sentiment']])
 writer.writerow([overallResult['chunk'], overallResult['keywords'], overallResult['sentiment']])
